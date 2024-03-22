dd749afbf2890ddccc17624a6318bc704026712b
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2024-03-22 09:40:22 +0200

    finish

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -3,8 +3,8 @@ import css from './Description.module.css'
 const Description = () => {
     return (
         <div>
-         <h1>Sip Happens Caf&eacute;</h1>
-             <p>Please leave your feedback about our service by selecting one  of the options below.</p>
+         <h1 className={css.header}>Sip Happens Caf&eacute;</h1>
+             <p className={css.header_descr}>Please leave your feedback about our service by selecting one  of the options below.</p>
         </div>
     )
 
